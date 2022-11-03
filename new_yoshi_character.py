698786c4c506736067b70628a8d7707603035b31
froggorf <kjggr123@gmail.com>
2022-11-03 23:15:01 +0900

    리팩토링 준비 완료

--- new_yoshi_character.py
+++ new_yoshi_character.py
@@ -279,3 +279,183 @@ class Yoshi:
                 self.cur_state.enter(self)
         else:
             self.delay += 1
+
+    def draw(self):
+        self.cur_state.draw(self)
+
+    def update(self):
+        self.sprite_update()
+        self.calc_gravity()
+        self.calc_jump()
+        self.move()
+        self.check_camera()
+
+        self.cur_state.do(self)
+
+    def check_camera(self):
+        from play_state import stageState
+        if self.state == "MARIO":
+            self.camera[X] = stageState.cameraSize[X] // 2
+            self.camera[Y] = stageState.cameraSize[Y] // 2
+            if self.x < stageState.cameraSize[X] // 2:  # 맵 왼편
+                self.camera[X] = self.x
+            if self.y < stageState.cameraSize[Y] // 2:  # 맵 아래편
+                self.camera[Y] = self.y
+
+            # 맵 오른편 위편
+            if self.x > stageState.image[stageState.selectStage].w - stageState.cameraSize[X] // 2:
+                self.camera[X] = stageState.cameraSize[X] - (stageState.image[stageState.selectStage].w - self.x)
+            if self.y > stageState.image[stageState.selectStage].h - stageState.cameraSize[Y] // 2:
+                self.camera[Y] = stageState.cameraSize[Y] - (stageState.image[stageState.selectStage].h - self.y)
+
+    def calc_jump(self):#TODO: 점프중에만 적용되도록 설정
+        from play_state import stageState
+        #TODO: 나중에 game_world 에 넣어서 한번에 꺼내쓸수 있도록 해보기
+        for rect in stageState.largerBlock:
+            if self.myIntersectRect(rect.pos):
+                self.y = rect.pos.bottom - self.size[Y] - 1
+                self.gravity = 0
+                rect.larger_block = True
+                #TODO: 나중에 larger_block 상태 만들고서 치면 바뀌게 하기
+        for rect in stageState.footBlock:
+            if self.myIntersectRect(rect.pos):
+                self.y = rect.pos.bottom - self.size[Y] - 1
+                self.gravity = 0
+        for rect in stageState.ceilingBlock:
+            if self.myIntersectRect(rect):
+                self.y = rect.bottom - self.size[Y] - 2
+                self.gravity = 0
+
+    def calc_gravity(self):
+        global GRAVITY
+        self.y += self.gravity
+        self.gravity -= GRAVITY
+        if self.gravity < -GRAVITY * 3:
+            self.gravity = -GRAVITY * 3
+        #TODO: check_fall 함수 이름을 check_landing 으로 바꾸기
+        from play_state import stageState
+        for rect in stageState.jumpBlock:
+            if self.y - self.gravity >= rect.pos.top:
+                if self.myIntersectRect(rect.pos):
+                    self.y = rect.pos.top
+                    self.gravity = rect.jump_power
+        for rect in stageState.groundRect:
+            if self.myIntersectRect(rect):
+                self.y = rect.top
+                self.gravity = -GRAVITY;
+                if self.cur_state == FALL:
+                    self.check_fall()
+        for rect in stageState.stairRect:
+            if self.myIntersectRect(rect):
+                self.y = rect.top
+                self.gravity = -GRAVITY;
+                if self.cur_state == FALL:
+                    self.check_fall()
+        for rect in stageState.largerBlock:
+            if self.myIntersectRect(rect.pos):
+                self.y = rect.pos.top
+                self.gravity = -GRAVITY
+                if self.cur_state == FALL:
+                    self.check_fall()
+        for rect in stageState.footBlock:
+            if self.myIntersectRect(rect.pos):
+                self.y = rect.pos.top
+                self.gravity = -GRAVITY;
+                if self.cur_state == FALL:
+                    self.check_fall()
+
+    def check_fall(self):
+        if self.dir[X] == 0:
+            self.cur_state.exit(self)
+            self.cur_state = IDLE
+            self.cur_state.enter(self)
+        else:
+            self.cur_state.exit(self)
+            self.cur_state = WALK
+            self.cur_state.enter(self)
+
+    def myIntersectRect(self, rect ):
+        if rect.left == -1: return
+        bVertical = False
+        bHorizontal = False
+
+        if self.x < rect.right and self.x+self.size[X] > rect.left:
+            bHorizontal=True
+
+        if self.y+self.size[Y] > rect.bottom and self.y < rect.top:
+            bVertical = True
+
+        if bVertical and bHorizontal:
+            return True
+        else:
+            return False
+
+    def move(self):
+        self.x += self.dir[X] * self.speed
+        if self.cur_state==RUN:
+            self.x += self.dir[X] * self.speed // 2
+        from play_state import stageState
+        for rect in stageState.groundRect:
+            if self.myIntersectRect(rect):
+                if self.dir[X] == 1:
+                    self.x = rect.left - self.size[X]
+                elif self.dir[X] == -1:
+                    self.x = rect.right
+        for rect in stageState.largerBlock:
+            if self.myIntersectRect(rect.pos):
+                if self.dir[X] == 1:
+                    self.x = rect.pos.left - self.size[X]
+                elif self.dir[X] == -1:
+                    self.x = rect.pos.right
+        for rect in stageState.footBlock:
+            if self.myIntersectRect(rect.pos):
+                if self.dir[X] == 1:
+                    self.x = rect.pos.left - self.size[X]
+                elif self.dir[X] == -1:
+                    self.x = rect.pos.right
+        if self.x < 0:
+            self.x = 0
+        if self.y < 0:
+            self.y = 0
+
+
+        #TODO: 나중에 다시 읽고 리팩토링이 되도록 설정하기
+    def check_jump(self):
+        global GRAVITY, MAXJUMPTIME, jump_delay
+        if self.pressJump != 0:
+            if self.pressJump <=MAXJUMPTIME:
+                if jump_delay == 0:
+                    if self.gravity==-GRAVITY: self.gravity+=GRAVITY*4
+                    else :self.gravity += GRAVITY*2
+                    self.pressJump+=1
+                jump_delay = (jump_delay+1) % 3
+            else:
+                self.pressJump=0
+                jump_delay=0
+                if self.motion == "LEFT_JUMP":
+                    self.change_motion("LEFT_FALL")
+                    self.cur_state.exit(self)
+                    self.cur_state= FALL
+                    self.cur_state.enter(self)
+                elif self.motion == "RIGHT_JUMP":
+                    self.change_motion("RIGHT_FALL")
+                    self.cur_state.exit(self)
+                    self.cur_state = FALL
+                    self.cur_state.enter(self)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
+        if self.event_que:
+            event = self.event_que.pop()
+            if event in next_state[self.cur_state]:
+                self.cur_state.exit(self, event)
+                self.cur_state = next_state[self.cur_state][event]
+                self.cur_state.enter(self, event)
+
+

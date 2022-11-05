83db82eb391274575af8334bf3e986cee77114c1
froggorf <kjggr123@gmail.com>
2022-11-05 14:38:32 +0900

    캐릭터 상태 리팩토링 완성

--- yoshi_character.py
+++ yoshi_character.py
@@ -2,15 +2,6 @@ from pico2d import *
 import random
 
 yoshi_state = {"MARIO": 0 , "NOMARIO":1, "MARIO_SWALLOW":2, "NOMARIO_SWALLOW":3}
-yoshi_motion = {"RIGHT_IDLE_01":0, "LEFT_IDLE_01":1,"RIGHT_IDLE_02":2,"LEFT_IDLE_02":3, "RIGHT_WALK":4,"LEFT_WALK":5, "RIGHT_RUN":6, "LEFT_RUN":7, "RIGHT_JUMP":10, "LEFT_JUMP":11, "RIGHT_FALL":12, "LEFT_FALL":13}
-yoshi_offset = [
-    [(int(62*1.6),int(66*1.6)),(int(62*1.6),int(66*1.6)),(int(62*1.6),int(64*1.6)),(int(62*1.6),int(64*1.6)),(int(64*1.6),int(66*1.6)),(int(64*1.6),int(66*1.6)),(int(72*1.6),int(1.6*68)),(int(72*1.6),int(1.6*68)),(),(),(int(60*1.6),int(72*1.6)),(int(60*1.6),int(72*1.6)),(int(58*1.6),int(62*1.6)),(int(58*1.6),int(62*1.6))],
-    [5],
-    [5],
-    [5]
-]
-yoshi_delay = [8,8,10,10,8,8,8,8,8,0,0,0,0,0,0]
-yoshi_motion_num = [8,8,5,5,8,8,2,2,0,0,1,1,1,1]
 X = 0
 Y = 1
 LEFT = 0
@@ -21,6 +12,7 @@ jump_delay = 0
 
 #이벤트 정의
 WD,WU,AD,AU,SD,SU,DD,DU,SHIFTD,SHIFTU = range(10)
+key_down = [False for i in range(10)]
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_w): WD,
@@ -35,16 +27,14 @@ key_event_table = {
     (SDL_KEYUP, SDLK_LSHIFT): SHIFTU,
 }
 
-class IDLE:
+class IDLE_01:
     def enter(self, event=None):
         self.delay = 0
         self.frame = 0
         self.dir[X] = 0
-        print('enter - IDLE')
         pass
 
     def exit(self, event=None):
-        print('exit - IDLE')
         pass
 
     def do(self):
@@ -69,8 +59,39 @@ class IDLE:
                 self.camera[X] + int(62*1.6) // 2,
                 self.camera[Y] + int(66*1.6) // 2
             )
-            pass
+
+class IDLE_02:
+    def enter(self, event=None):
+        self.delay = 0
+        self.frame = 0
+        self.dir[X] = 0
         pass
+
+    def exit(self, event=None):
+        pass
+
+    def do(self):
+        pass
+
+    def draw(self):
+        if self.face == RIGHT: #RIGHT
+            self.image[yoshi_state[self.state]].clip_draw(
+                int(62*1.6) * self.frame,
+                320,
+                int(62*1.6),
+                int(66*1.6),
+                self.camera[X] + int(62*1.6) // 2,
+                self.camera[Y] + int(66*1.6) // 2
+            )
+        else:
+            self.image[yoshi_state[self.state]].clip_draw(
+                int(62*1.6) * self.frame,
+                480,
+                int(62 * 1.6),
+                int(66 * 1.6),
+                self.camera[X] + int(62*1.6) // 2,
+                self.camera[Y] + int(66*1.6) // 2
+            )
 
 class WALK:
     def enter(self, event=None):
@@ -89,7 +110,6 @@ class WALK:
             self.dir[X] += 1
             self.face = RIGHT
 
-        pass
 
     def exit(self, event=None):
         pass
@@ -117,7 +137,6 @@ class WALK:
                 self.camera[X] + int(64*1.6) // 2,
                 self.camera[Y] + int(66*1.6) // 2
             )
-        pass
 
 class RUN:
     def enter(self, event=None):
@@ -151,39 +170,39 @@ class RUN:
                 self.camera[X] + int(72*1.6) // 2,
                 self.camera[Y] + int(68*1.6)// 2
             )
-        pass
 
 class JUMP:
     def enter(self, event=None):
         self.delay = 0
         self.frame = 0
+        if event == WD:
+            self.gravity += GRAVITY*5
+            self.pressJump += 1
+
         if event == DD:
-            self.dir[X] += 1
+            self.dir[X]+=1
             self.face = RIGHT
         elif event == AD:
-            self.dir[X] -= 1
+            self.dir[X]-=1
             self.face = LEFT
         elif event == DU:
             self.dir[X] -= 1
-            self.face = LEFT
+            if key_down[AD] == True:
+                self.face = LEFT
         elif event == AU:
             self.dir[X] += 1
-            self.face = RIGHT
-        self.gravity += GRAVITY * 4
-        self.pressJump += 1
-        pass
+            if key_down[DD] == True:
+                self.face= RIGHT
 
     def exit(self, event=None):
-        self.pressJump = 0
+        if event == WU:
+            self.pressJump = 0
+
         pass
 
     def do(self):
-        if self.gravity < -GRAVITY:
-            self.cur_state.exit(self)
-            self.cur_state = FALL
-            self.cur_state.enter(self)
-
         self.check_jump()
+
         pass
 
     def draw(self):
@@ -205,17 +224,28 @@ class JUMP:
                 self.camera[X] + int(60 * 1.6) // 2,
                 self.camera[Y] + int(72 * 1.6) // 2
             )
-        pass
 
 class FALL:
     def enter(self, event=None):
-        print('enter - FALL')
         self.delay = 0
         self.frame = 0
+        if event == DD:
+            self.dir[X]+=1
+            self.face = RIGHT
+        elif event == AD:
+            self.dir[X]-=1
+            self.face = LEFT
+        elif event == DU:
+            self.dir[X] -= 1
+            if key_down[AD] == True:
+                self.face = LEFT
+        elif event == AU:
+            self.dir[X] += 1
+            if key_down[DD] == True:
+                self.face= RIGHT
         pass
 
     def exit(self, event=None):
-        print('exit - FALL')
         pass
 
     def do(self):
@@ -242,75 +272,75 @@ class FALL:
             )
 
 next_state = {
-    IDLE: {WD: JUMP, AD: WALK, AU: WALK, DD: WALK, DU: WALK},
-    WALK: {WD: JUMP, AD: IDLE, AU: IDLE, DD: IDLE, DU: IDLE, SHIFTD: RUN, SHIFTU: RUN},
-    RUN: {WD: JUMP, AD: IDLE, AU: IDLE, DD: IDLE, DU: IDLE, SHIFTD: WALK, SHIFTU: WALK},
-    JUMP: {AD:JUMP,AU:JUMP,DD:JUMP,DU:JUMP},
+    IDLE_01: {WD: JUMP, AD: WALK, AU: WALK, DD: WALK, DU: WALK},
+    IDLE_02: {WD: JUMP, AD: WALK, AU: WALK, DD: WALK, DU: WALK},
+    WALK: {WD: JUMP, AD: IDLE_01, AU: IDLE_01, DD: IDLE_01, DU: IDLE_01, SHIFTD: RUN, SHIFTU: WALK},
+    RUN: {WD: JUMP, AD: IDLE_01, AU: IDLE_01, DD: IDLE_01, DU: IDLE_01, SHIFTD: WALK, SHIFTU: WALK},
+    JUMP: {WD: JUMP, WU:FALL,AD: JUMP, AU: JUMP, DD: JUMP, DU: JUMP},
     FALL: {AD: FALL, AU: FALL, DD: FALL, DU: FALL}
 }
+yoshi_delay = {IDLE_01:8,IDLE_02:10,WALK:8,RUN: 8,JUMP:0,FALL:0}
+yoshi_motion_num = {IDLE_01:8,IDLE_02:5, WALK:8, RUN:2, JUMP:1, FALL:1}
+
 
 class Yoshi:
     def __init__(self):
         self.image = [load_image("yoshi_mario.png")]
-        #위치 관련
+        # 위치 관련
         self.x = 4000
         self.y = 1500
-        self.size=[int(62*1.6),int(66*1.6)]
+        self.size = [int(62 * 1.6), int(66 * 1.6)]
 
-        #상태 관련
+        # 상태 관련
         self.state = "MARIO"
         self.frame = 0
         self.delay = 0
 
-        #이동 관련
+        # 이동 관련
         self.dir = [0, 0]
         self.speed = 5
         self.gravity = -GRAVITY
 
-        #카메라 관련
-        self.camera = [0,0]
+        # 카메라 관련
+        self.camera = [0, 0]
 
-        #점프 관련
+        # 점프 관련
         self.pressJump = False
 
-        #상태 관련(리팩토링중)
+        # 상태 관련(리팩토링중)
         self.event_que = []
-        self.cur_state = IDLE
+        self.cur_state = IDLE_01
         self.face = LEFT
         self.cur_state.enter(self)
 
-    #그리기 관련 함수
     def sprite_update(self):
-        if self.delay >= yoshi_delay[yoshi_motion[self.motion]] :
-            self.frame = (self.frame+1)%yoshi_motion_num[yoshi_motion[self.motion]]
+        if self.delay >= yoshi_delay[self.cur_state] :
+            self.frame = (self.frame+1)%yoshi_motion_num[self.cur_state]
             self.delay = 0
-            if (self.motion == "RIGHT_IDLE_01" or self.motion == "RIGHT_IDLE_02") and self.frame == 0:
+            if self.cur_state==IDLE_01 and self.frame == 0:
                 if random.randint(0, 3) == 0:
-                    self.change_motion("RIGHT_IDLE_02")
-                else:
-                    self.change_motion("RIGHT_IDLE_01")
-            elif (self.motion == "LEFT_IDLE_01" or self.motion == "LEFT_IDLE_02") and self.frame == 0:
-                if random.randint(0, 3) == 0:
-                    self.change_motion("LEFT_IDLE_02")
-                else:
-                    self.change_motion("LEFT_IDLE_01")
+                    self.cur_state.exit(self)
+                    self.cur_state = IDLE_02
+                    self.cur_state.enter(self)
+            if self.cur_state==IDLE_02 and self.frame == 0:
+                self.cur_state.exit(self)
+                self.cur_state = IDLE_01
+                self.cur_state.enter(self)
         else:
             self.delay += 1
 
     def draw(self):
         self.cur_state.draw(self)
-        pass
-
-
 
     def update(self):
         self.sprite_update()
+
+        self.cur_state.do(self)
         self.calc_gravity()
-        self.calc_jump()
+        self.check_block()
         self.move()
         self.check_camera()
 
-        self.cur_state.do(self)
 
     def check_camera(self):
         from play_state import stageState
@@ -328,44 +358,37 @@ class Yoshi:
             if self.y > stageState.image[stageState.selectStage].h - stageState.cameraSize[Y] // 2:
                 self.camera[Y] = stageState.cameraSize[Y] - (stageState.image[stageState.selectStage].h - self.y)
 
- 
-    def calc_jump(self):
+    def check_block(self):#TODO: 점프중에만 적용되도록 설정
         from play_state import stageState
-        for rect in stageState.largerBlock:
-            if self.myIntersectRect(rect.pos):
-                self.y = rect.pos.bottom - self.size[Y]-1
-                self.gravity = 0
-                rect.larger_block=True
-        for rect in stageState.footBlock:
-            if self.myIntersectRect(rect.pos):
-                self.y = rect.pos.bottom - self.size[Y]-1
-                self.gravity = 0
-        for rect in stageState.ceilingBlock:
-            if self.myIntersectRect(rect):
-                self.y = rect.bottom - self.size[Y]-2
-                self.gravity= 0
-        
+        #TODO: 나중에 game_world 에 넣어서 한번에 꺼내쓸수 있도록 해보기
+        if self.gravity>=0:
+            for rect in stageState.largerBlock:
+                if self.myIntersectRect(rect.pos):
+                    self.y = rect.pos.bottom - self.size[Y] - 1
+                    self.gravity = 0
+                    rect.larger_block = True
+                    #TODO: 나중에 larger_block 상태 만들고서 치면 바뀌게 하기
+            for rect in stageState.footBlock:
+                if self.myIntersectRect(rect.pos):
+                    self.y = rect.pos.bottom - self.size[Y] - 1
+                    self.gravity = 0
+            for rect in stageState.ceilingBlock:
+                if self.myIntersectRect(rect):
+                    self.y = rect.bottom - self.size[Y] - 2
+                    self.gravity = 0
 
-    def calc_gravity(self):
-        global GRAVITY
-        self.y += self.gravity
-        self.gravity -= GRAVITY
-        if self.gravity < -GRAVITY*3:
-            self.gravity = -GRAVITY*3
-
-        from play_state import stageState
-        if self.gravity<=0:     #중력 적용중일때
+        # TODO: check_fall 함수 이름을 check_landing 으로 바꾸기
+        else:
             for rect in stageState.jumpBlock:
-                if self.y-self.gravity>=rect.pos.top:
+                if self.y - self.gravity >= rect.pos.top:
                     if self.myIntersectRect(rect.pos):
                         self.y = rect.pos.top
                         self.gravity = rect.jump_power
-
             for rect in stageState.groundRect:
                 if self.myIntersectRect(rect):
                     self.y = rect.top
-                    self.gravity=-GRAVITY;
-                    if self.cur_state==FALL:
+                    self.gravity = -GRAVITY;
+                    if self.cur_state == FALL:
                         self.check_fall()
             for rect in stageState.stairRect:
                 if self.myIntersectRect(rect):
@@ -387,16 +410,24 @@ class Yoshi:
                         self.check_fall()
 
 
+    def calc_gravity(self):
+        self.y += self.gravity
+        self.gravity -= GRAVITY
+        if self.gravity < -GRAVITY * 3:
+            self.gravity = -GRAVITY * 3
+
+
     def check_fall(self):
         if self.dir[X] == 0:
             self.cur_state.exit(self)
-            self.cur_state = IDLE
+            self.cur_state = IDLE_01
             self.cur_state.enter(self)
-        else:
+        elif key_down[AD] or key_down[DD]:
             self.cur_state.exit(self)
             self.cur_state = WALK
             self.cur_state.enter(self)
-
+        else:
+            print('Error - 일어날 수 없음')
 
     def myIntersectRect(self, rect ):
         if rect.left == -1: return
@@ -416,8 +447,8 @@ class Yoshi:
 
     def move(self):
         self.x += self.dir[X] * self.speed
-        if self.motion == "RIGHT_RUN" or self.motion == "LEFT_RUN":
-            self.x += self.dir[X] * self.speed//2
+        if self.cur_state==RUN:
+            self.x += self.dir[X] * self.speed // 2
         from play_state import stageState
         for rect in stageState.groundRect:
             if self.myIntersectRect(rect):
@@ -437,13 +468,15 @@ class Yoshi:
                     self.x = rect.pos.left - self.size[X]
                 elif self.dir[X] == -1:
                     self.x = rect.pos.right
-        if self.x<0:
+        if self.x < 0:
             self.x = 0
-        if self.y<0:
-            self.y=0
+        if self.y < 0:
+            self.y = 0
 
+
+        #TODO: 나중에 다시 읽고 리팩토링이 되도록 설정하기
     def check_jump(self):
-        global GRAVITY,MAXJUMPTIME, jump_delay
+        global jump_delay
         if self.pressJump != 0:
             if self.pressJump <=MAXJUMPTIME:
                 if jump_delay == 0:
@@ -454,19 +487,13 @@ class Yoshi:
             else:
                 self.pressJump=0
                 jump_delay=0
-                if self.motion == "LEFT_JUMP":
-                    self.change_motion("LEFT_FALL")
-                    self.cur_state.exit(self)
-                    self.cur_state= FALL
-                    self.cur_state.enter(self)
-                elif self.motion == "RIGHT_JUMP":
-                    self.change_motion("RIGHT_FALL")
-                    self.cur_state.exit(self)
-                    self.cur_state = FALL
-                    self.cur_state.enter(self)
 
-    def add_event(self,event):
-        self.event_que.insert(0,event)
+                self.cur_state.exit(self)
+                self.cur_state= FALL
+                self.cur_state.enter(self)
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
@@ -475,7 +502,33 @@ class Yoshi:
 
         if self.event_que:
             event = self.event_que.pop()
+            set_keydown(event)
             if event in next_state[self.cur_state]:
-                self.cur_state.exit(self,event)
+                self.cur_state.exit(self, event)
                 self.cur_state = next_state[self.cur_state][event]
-                self.cur_state.enter(self,event)
+                self.cur_state.enter(self, event)
+
+#TODO: 좋은 방법 생각나면 리팩토링 하기
+def set_keydown(event):
+    if event == WD:
+        key_down[WD]=True
+    elif event==WU:
+        key_down[WD] = False
+    elif event == AD:
+        key_down[AD] = True
+    elif event == AU:
+        key_down[AD] = False
+    elif event == SD:
+        key_down[SD] = True
+    elif event == SU:
+        key_down[SD] = False
+    elif event == DD:
+        key_down[DD] = True
+    elif event == DU:
+        key_down[DD] = False
+    elif event == SHIFTD:
+        key_down[SHIFTD] = True
+    elif event == SHIFTU:
+        key_down[SHIFTD] = False
+
+

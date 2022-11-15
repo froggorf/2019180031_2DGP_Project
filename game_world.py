f1c0cd80bc7ac61b29ff915c922755e093233e03
froggorf <kjggr123@gmail.com>
2022-11-15 23:17:19 +0900

    game_world 파일 추가

---
+++ game_world.py
@@ -0,0 +1,76 @@
+
+# layer 0: Background Objects
+# layer 1: Foreground Objects
+objects = [[], []]
+#'boy:ball' : [(boy),(ball1, ball2, ball3, ...)] 와 같은 정보가 담김
+collision_group = dict()
+
+def add_object(o, depth):
+    objects[depth].append(o)
+
+def add_objects(ol, depth):
+    objects[depth] += ol
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            #충돌 그룹에서도, 객체를 리스트에서 삭제해야함
+            remove_collision_object(o)
+            del o
+            return
+    raise ValueError('Trying destroy non existing object')
+
+
+# def remove_object(o):
+#     for layer in objects:
+#         try:
+#             layer.remove(o)
+#             del o
+#             return
+#         except:
+#             pass
+#     raise ValueError('Trying destroy non existing object')
+
+
+def all_objects():
+    for layer in objects:
+        for o in layer:
+            yield o
+
+
+def clear():
+    for o in all_objects():
+        del o
+    for layer in objects:
+        layer.clear()
+
+def add_collision_group(a,b,group):
+    if group not in collision_group:
+        print('New Group Made')
+        collision_group[group] = [[], []]
+    if a:
+        if type(a) == list:
+            collision_group[group][0] += a
+        else:
+            collision_group[group][0].append(a)
+
+    if b:
+        if type(b) == list:
+            collision_group[group][1] += b
+        else:
+            collision_group[group][1].append(b)
+
+def all_collision_pairs():
+    #collision_group 딕셔너리에서 각 리스트로부터 페어를 만들어서 보내줌
+    for group, pairs in collision_group.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                yield a, b, group
+
+def remove_collision_object(o):
+    for pairs in collision_group.values(): #key:value 에서 value에 해당되는것만 가져옴
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        elif o in pairs[1]:
+            pairs[1].remove(o)
